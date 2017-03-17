65447d3c9d60605720f11115039497db74787cbd
erpachecomo <erpachecomo@ittepic.edu.mx>
2017-03-16 21:12:11 -0700

    primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { WelcomePage } from './../pages/welcome/welcome';
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    WelcomePage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    WelcomePage
+  ],
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+})
+export class AppModule {}
