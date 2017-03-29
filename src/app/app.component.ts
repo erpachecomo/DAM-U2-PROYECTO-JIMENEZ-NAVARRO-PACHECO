6eb9fcd87a91093ddbc1e3e7e1a830fdea252a70
erpachecomo <erpachecomo@ittepic.edu.mx>
2017-03-29 14:29:07 -0700

    Login redes sociales falta testear

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
+
 import { WelcomePage } from './../pages/welcome/welcome';
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar, Splashscreen } from 'ionic-native';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav } from 'ionic-angular';
+import { StatusBar,NativeStorage,GooglePlus, Splashscreen } from 'ionic-native';
+import { HomePage } from '../pages/home/home';
 
 
 
@@ -9,14 +11,41 @@ import { StatusBar, Splashscreen } from 'ionic-native';
   templateUrl: 'app.html'
 })
 export class MyApp {
+  @ViewChild(Nav) nav: Nav;
+
   rootPage = WelcomePage;
 
-  constructor(platform: Platform) {
+    constructor(platform: Platform) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      StatusBar.styleDefault();
       Splashscreen.hide();
+      StatusBar.styleDefault();
+        
+      
     });
   }
+   ngAfterViewInit(){
+      //this.nav is now defined
+      setTimeout(() => {
+      let env = this;
+
+      GooglePlus.trySilentLogin({
+        'scopes': '', // optional, space-separated list of scopes, If not included or empty, defaults to `profile` and `email`.
+        'webClientId': '19993281606-npcoekmi8kje4ljdj3or2c0q3blhnnee.apps.googleusercontent.com', 
+        // optional clientId of your Web application from Credentials settings of your project - On Android, this MUST be included to get an idToken. On iOS, it is not required.
+        'offline': true
+      })
+
+
+
+      NativeStorage.getItem('user')
+      .then( function (data) {
+        // user is previously logged and we have his data
+        // we will let him access the app
+        env.nav.push(HomePage);
+      }, function (error) {
+        //we don't have the user data so we will ask him to log in
+        env.nav.push(WelcomePage);
+      });
+      }, 2000);
+    }
 }
--- src/pages/welcome/welcome.ts
+++ src/pages/welcome/welcome.ts
@@ -1,6 +1,7 @@
 import { HomePage } from './../home/home';
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,LoadingController } from 'ionic-angular';
+import { Facebook, NativeStorage,GooglePlus } from 'ionic-native';
 import { Validators, FormBuilder, FormGroup } from '@angular/forms'
 
 /*
@@ -22,8 +23,11 @@ export class WelcomePage {
   public name:any;
   public email:any;
   public password:any;
-
-  constructor(public navCtrl: NavController, private formBuilder: FormBuilder) {
+  FB_APP_ID: number = 1750492968544998;
+  
+  
+  constructor(public navCtrl: NavController, private formBuilder: FormBuilder,public loadingCtrl:LoadingController) {
+        Facebook.browserInit(this.FB_APP_ID, "v2.8");       
     let emailRegex = '^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$';
     let specialRegex = /[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/;
 
@@ -66,5 +70,76 @@ export class WelcomePage {
   }
   ionViewDidLoad() {
   }
+  doFbLogin(){
+    console.log("Hello");
+    let permissions = new Array();  
+    let nav = this.navCtrl;
+    //the permissions your facebook app needs from the user
+    permissions = ["public_profile"];
+
+      console.log(JSON.stringify(permissions));
+
+    Facebook.login(permissions)
+    .then(function(response){
+      console.log(JSON.stringify(response));
+      let userId = response.authResponse.userID;
+      let params = new Array();
+
+      //Getting name and gender properties
+      Facebook.api("/me?fields=name,gender", params)
+      .then(function(user) {
+      
+
+        user.picture = "https://graph.facebook.com/" + userId + "/picture?type=large";
+        //now we have the users info, let's save it in the NativeStorage
+        NativeStorage.setItem('user',
+        {
+          name: user.name,
+          gender: user.gender,
+          picture: user.picture
+        })
+        .then(function(){
+          console.log("HOMEPAGE");
+          nav.push(HomePage);
+        }, function (error) {
+          console.log(JSON.stringify(error));
+        })
+      })
+    }, function(error){
+      console.log(JSON.stringify(error));
+    });
+  }
+
+  doGoogleLogin(){
+  let nav = this.navCtrl;
+  let loading = this.loadingCtrl.create({
+    content: 'Please wait...'
+  });
+  loading.present();
+  GooglePlus.login({
+    'scopes': '', // optional, space-separated list of scopes, If not included or empty, defaults to `profile` and `email`.
+    'webClientId': '19993281606-npcoekmi8kje4ljdj3or2c0q3blhnnee.apps.googleusercontent.com', 
+    'offline': true
+  })
+  .then(function (user) {
+    loading.dismiss();
+
+    NativeStorage.setItem('user', {
+      name: user.displayName,
+      email: user.email,
+      picture: user.imageUrl
+    })
+    .then(function(){
+      nav.push(HomePage);
+    }, function (error) {
+      console.log('Algo paso');
+      console.log(JSON.stringify(error));
+    })
+  }, function (error) {
+    console.log(JSON.stringify(error));
+    loading.dismiss();
+  });
+}
+
 
 }
