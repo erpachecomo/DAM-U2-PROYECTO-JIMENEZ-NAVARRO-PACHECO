8bc0ec9e0a678aea7131292aab59bbdb03b1818b
erpachecomo <erpachecomo@ittepic.edu.mx>
2017-03-30 19:36:39 -0700

    Login con logo y funcionaimento de fb y google

--- src/pages/welcome/welcome.ts
+++ src/pages/welcome/welcome.ts
@@ -2,7 +2,6 @@ import { HomePage } from './../home/home';
 import { Component } from '@angular/core';
 import { NavController,LoadingController } from 'ionic-angular';
 import { Facebook, NativeStorage,GooglePlus } from 'ionic-native';
-import { Validators, FormBuilder, FormGroup } from '@angular/forms'
 
 /*
   Generated class for the Welcome page.
@@ -15,58 +14,24 @@ import { Validators, FormBuilder, FormGroup } from '@angular/forms'
   templateUrl: 'welcome.html'
 })
 export class WelcomePage {
-  public register:any;
-  public showRegister:boolean=false;
-  public wrongData:boolean=false;
-
-  public myForm: FormGroup;
-  public name:any;
-  public email:any;
-  public password:any;
   FB_APP_ID: number = 1750492968544998;
   
   
-  constructor(public navCtrl: NavController, private formBuilder: FormBuilder,public loadingCtrl:LoadingController) {
-        Facebook.browserInit(this.FB_APP_ID, "v2.8");       
-    let emailRegex = '^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$';
-    let specialRegex = /[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/;
-
-    this.myForm = this.formBuilder.group({
-      name: ['', Validators.required],
-      register: [false],
-      password: ['', [Validators.required,<any>Validators.pattern(specialRegex)]],
-      email: ['', [<any>Validators.pattern(emailRegex), Validators.required]],
-    });
-
-    this.name = this.myForm.controls['name'];
-    this.register = this.myForm.controls['register'];
-    this.password = this.myForm.controls['password'];
-    this.email = this.myForm.controls['email'];
-
-  }
-  containsSpecial():boolean{
-    return '\\!"#$%&/()=?¡-.,_:;{´+*[]}─|@·~½¬|°'.includes(this.password.value);
-  }
-  changed(){
-    this.showRegister=this.register.value;
+  constructor(public navCtrl: NavController, public loadingCtrl:LoadingController) {
+        Facebook.browserInit(this.FB_APP_ID, "v2.8");
   }
   continue(){
-    if((this.email.valid&&this.password.valid&&!this.showRegister&&!this.name.valid)||this.myForm.valid){
-      if(!this.showRegister){//Si es login
-        if(this.password.value==='!admin'&&this.email.value==='admin@example.com'){
-          //login correcto
-          this.wrongData=false;
-          this.navCtrl.push(HomePage);
-          return;
-        }else{
-          this.wrongData=true;
-          return;
-        }
-      }
-      this.navCtrl.push(HomePage);      
-      return;
-    }
-    this.wrongData=false;
+    let nav = this.navCtrl;
+     NativeStorage.setItem('user',
+        {
+          name: "invitado"
+        })
+        .then(function(){
+          console.log("HOMEPAGE");
+          nav.push(HomePage);
+        }, function (error) {
+          console.log(JSON.stringify(error));
+        })
   }
   ionViewDidLoad() {
   }
